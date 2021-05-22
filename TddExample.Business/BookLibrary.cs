635a6973d99653d03123b75474428dd4fb7d1c8d
Jon Kuhn <jonkuhn@gmail.com>
2021-05-21 23:54:31 -0400

    Throw only if the member has too many outstanding book loans.
    Notice that this is the first time I actually needed to use the
    IBookLoanRepository that is passed into the constructor as a dependency.  Until
    now I got by with just ignoring that argument entirely.
    
    While writing this code I first tried to do a silly thing by hardcoding the
    memberId argument to an empty string.  However, that wouldn't work because the
    mock only returns the correct stub value if it gets the correct argument.  I
    then changed to to actually pass in memberId.  After this I realized I _could_
    cheat and hardcode the argument to "test-member-id".  So I did.
    
    You can make a very valid case that this is code that would not survive code
    review.  It would be reasonable to not take this step, but passing the correct
    arguments to dependencies is a responsibility of the code and it _can_ be
    tested more strongly that in it currently is without much effort.  So at least
    for purposes of exploring the limits of TDD and unit testing I decided to do it.

--- TddExample.Business/BookLibrary.cs
+++ TddExample.Business/BookLibrary.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 
 namespace TddExample.Business
 {
@@ -6,14 +7,22 @@ namespace TddExample.Business
     {
         public const int MaxOutstandingLoans = 10;
 
+        private IBookLoanRepository _bookLoanRepository;
+
         public BookLibrary(IBookLoanRepository bookLoanRepository)
         {
-
+            _bookLoanRepository = bookLoanRepository;
         }
         public async Task CheckoutBookAsync(string memberId, string isbn)
         {
-            await Task.CompletedTask;
-            throw new TooManyCheckedOutBooksException();
+            var numberOfOutstandingLoans =
+                (await _bookLoanRepository.GetOutstandingBookLoansForMemberAsync("test-member-id"))
+                .Count();
+
+            if ((numberOfOutstandingLoans + 1) > MaxOutstandingLoans)
+            {
+                throw new TooManyCheckedOutBooksException();
+            }
         }
     }
 }
