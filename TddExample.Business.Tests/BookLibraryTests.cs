f9e5bdc21e68e3a1915e20656b18b13c5fb23b35
Jon Kuhn <jonkuhn@gmail.com>
2021-05-21 23:37:48 -0400

    Test that having fewer than the max outstanding loans does not throw.
    I quickly created a copy of the first test that that will make me implement
    a real check on the return value of GetOutstandingBookLoansForMember.
    
    There is a ton of code duplication between the two tests right now and it will
    be cleaned up, but for now I just decided to make more progress on the
    implementation.
    
    There could be some concern around this test not being complete as well.
    Afterall in the final implementation we would need to stub out more things in
    order to get the CheckoutBookAsync function we are testing to not throw.  That
    is true, but not a problem.  It is okay for the individual tests to evolve
    and even be deleted as we go.  Potentially deleting tests can sound wasteful,
    but often enough is learned by writing them that needed to be learned anyway
    that it is not too big of a deal.

--- TddExample.Business.Tests/BookLibraryTests.cs
+++ TddExample.Business.Tests/BookLibraryTests.cs
@@ -45,5 +45,40 @@ namespace TddExample.Business.Tests
             Assert.ThrowsAsync<TooManyCheckedOutBooksException>(async () =>
                 await bookLibrary.CheckoutBookAsync(memberId, isbnOfBookToCheckOut));
         }
+
+        [Test]
+        public void TestCheckoutBookAsync_GivenMemberNotOverBookLimit_Succeeds()
+        {
+            const string memberId = "test-member-id";
+            const string isbnOfBookToCheckOut = "test-isbn";
+
+            var bookLoanRepository = Substitute.For<IBookLoanRepository>();
+
+            var stubOutstandingLoans = new List<BookLoan>();
+
+            // Stub GetOutstandingBookLoansForMember to return one less
+            // that the maximum number of outstanding loans so this checkout
+            // should be allowed
+            for (var i = 0; i < BookLibrary.MaxOutstandingLoans - 1; i++)
+            {
+                stubOutstandingLoans.Add(
+                    new BookLoan
+                    {
+                        Id = $"test-book-loan-id-{i}",
+                        MemberId = memberId,
+                        Isbn = $"isbn-{i}",
+                        CopyId = $"copy-id-for-book-{i}",
+                        DueDate = new DateTime(2021, 1, 1, 0, 0, 0),
+                        WasReturned = false
+                    });
+            }
+            bookLoanRepository.GetOutstandingBookLoansForMemberAsync(memberId)
+                .Returns(stubOutstandingLoans);
+
+            var bookLibrary = new BookLibrary(bookLoanRepository);
+
+            Assert.DoesNotThrowAsync(async () =>
+                await bookLibrary.CheckoutBookAsync(memberId, isbnOfBookToCheckOut));
+        }
     }
 }
